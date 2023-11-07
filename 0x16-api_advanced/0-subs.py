dd665eec758c764fb3ea4650bd58f9db35070f24
EndrexAkoto <martinakoto3@gmail.com>
2023-11-07 00:41:43 -0800

    0-main.py

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+0-subs
+"""
+import requests
+
+def number_of_subscribers(subreddit):
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {'User-Agent': 'my_user_agent'}
+    
+    response = requests.get(url, headers=headers)
+    
+    if response.status_code == 200:
+        data = response.json()
+        subscribers = data['data']['subscribers']
+        return subscribers
+    else:
+        return 0
+
+if __name__ == '__main__':
+    import sys
+
+    if len(sys.argv) < 2:
+        print("Please pass an argument for the subreddit to search.")
+    else:
+        subreddit = sys.argv[1]
+        num_subscribers = number_of_subscribers(subreddit)
+        print("{:d}".format(num_subscribers))
+
