92c373e277121c552587e61b60035b425ecd94f8
EndrexAkoto <martinakoto3@gmail.com>
2023-11-07 10:02:05 -0800

    0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -4,18 +4,22 @@
 """
 import requests
 
+
 def number_of_subscribers(subreddit):
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {'User-Agent': 'my_user_agent'}
-    
+
     response = requests.get(url, headers=headers)
-    
+
     if response.status_code == 200:
         data = response.json()
         subscribers = data['data']['subscribers']
         return subscribers
     else:
         return 0
+
+# Two blank lines before this if block
+
 
 if __name__ == '__main__':
     import sys
@@ -26,3 +30,4 @@ if __name__ == '__main__':
         subreddit = sys.argv[1]
         num_subscribers = number_of_subscribers(subreddit)
         print("{:d}".format(num_subscribers))
+
