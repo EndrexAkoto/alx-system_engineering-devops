0b727b612c9d815eb2c760e88824893e03583686
EndrexAkoto <martinakoto3@gmail.com>
2023-11-07 03:50:53 -0800

    2-main.py

---
+++ 0x16-api_advanced/2-recurse.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+"""
+2-recurse
+"""
+import requests
+
+def recurse(subreddit, hot_list=[], after=None):
+    if after is None:
+        url = f"https://www.reddit.com/r/{subreddit}/hot.json"
+    else:
+        url = f"https://www.reddit.com/r/{subreddit}/hot.json?after={after}"
+
+    headers = {'User-Agent': 'my_user_agent'}
+
+    response = requests.get(url, headers=headers)
+
+    if response.status_code == 200:
+        data = response.json()
+        if 'data' in data and 'children' in data['data']:
+            posts = data['data']['children']
+            for post in posts:
+                hot_list.append(post['data']['title'])
+
+            after = data['data']['after']
+            if after is not None:
+                return recurse(subreddit, hot_list, after)
+            else:
+                return hot_list
+        else:
+            return None
+    else:
+        return None
+
+if __name__ == '__main':
+    import sys
+
+    if len(sys.argv) < 2:
+        print("Please pass an argument for the subreddit to search.")
+    else:
+        subreddit = sys.argv[1]
+        result = recurse(subreddit)
+        if result is not None:
+            print(len(result))
+        else:
+            print("None")
