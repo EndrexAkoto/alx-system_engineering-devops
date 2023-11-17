19b41a54976f24d6857aa44131f024cf009f46da
EndrexAkoto <martinakoto3@gmail.com>
2023-11-17 07:01:25 +0000

    0-the_sky_is_the_limit_not.pp

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,18 +1,16 @@
-# Traffic increase in Nginx server
-
 # Default file ULIMIT increase
 exec { 'fix--for-nginx':
-     # ULIMIT value modified
-     command => '/bin/sed -i "s/15/4096/" /etc/default/nginx',
-     #Sed command path specification
-     path => '/usr/local/bin/:/bin/',
+  # ULIMIT value modified
+  command => '/bin/sed -i "s/15/4096/" /etc/default/nginx',
+  # Sed command path specification
+  path    => '/usr/local/bin/:/bin/',
 }
 
 # Nginx restart
 exec { 'nginx-restart':
-     # Nginx restart service
-     command => '/etc/init.d/nginx restart',
-     # Init.d path specification
-     path => '/etc/init.d/',
+  # Nginx restart service
+  command => '/etc/init.d/nginx restart',
+  # Init.d path specification
+  path    => '/etc/init.d/',
 }
 
